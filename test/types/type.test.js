dd130b41300d256b3d8a8c4a2b678710bd99ebf9
Rob Cameron <rob.cameron@fastmail.com>
2020-09-09 10:21:26 -0700

    Adds tests for Type

--- test/types/type.test.js
+++ test/types/type.test.js
@@ -1,3 +1,36 @@
-test('truth', () => {
-  expect(true).toEqual(true)
+import { API_ENDPOINT } from '../../src/repeater'
+import { GraphQLClient } from 'graphql-request'
+import Type from '../../src/types/type'
+
+jest.mock('graphql-request')
+
+beforeEach(() => {
+  GraphQLClient.mockClear()
+})
+
+test('constructor() saves the given token', () => {
+  const type = new Type({}, { token: 'abc' })
+
+  expect(type.token).toEqual('abc')
+  expect(type.options).toEqual({})
+})
+
+test('constructor() saves any additional options', () => {
+  const type = new Type({}, { token: 'abc', foo: 'bar' })
+
+  expect(type.options).toEqual({ foo: 'bar' })
+})
+
+test('constructor() saves the passed data', () => {
+  const type = new Type({ foo: 'bar' }, { token: 'abc' })
+
+  expect(type.data).toEqual({ foo: 'bar' })
+})
+
+test('constructor() initializes the GraphQL client', () => {
+  new Type({}, { token: 'abc', endpoint: 'http://test.host' })
+
+  expect(GraphQLClient).toHaveBeenCalledWith('http://test.host', {
+    headers: { authorization: `Bearer abc` },
+  })
 })
